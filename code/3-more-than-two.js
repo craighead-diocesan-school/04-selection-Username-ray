54b12a65bf0e5e4baf9c0c87715bd98e256310b6
Username-ray <maedara96@craighead.school.nz>
2023-06-19 09:56:23 +1200

    Completed task3 Extention that keep asking number while don't say Stop!

--- code/3-more-than-two.js
+++ code/3-more-than-two.js
@@ -3,5 +3,22 @@
 // ####################################
 
 function moreThanTwo() {
-  // write your code here
+
+  let goOrNot = `dfghjftynki`
+  while (goOrNot != 'Stop!') {
+
+    let compared = prompt(`What number do you want to compare to 2?`)
+    compared = Number(compared)
+
+    if (compared >= 2) {
+      alert(compared + ' is more than 2')
+    } else {
+      alert(compared + ' is not more than 2')
+    }
+
+    goOrNot = prompt("Do you want to keep going or stop? If you want to stop, write down 'Stop!'")
+  }
+
+  alert('Okay.')
+
 }
