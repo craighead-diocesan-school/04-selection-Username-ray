89da7d75d8942700dc31dd3a69ddbe5e8bb62605
Username-ray <maedara96@craighead.school.nz>
2023-07-24 09:39:28 +1200

    Added comments to explain what the codes doing

--- code/1-driving-age.js
+++ code/1-driving-age.js
@@ -5,6 +5,7 @@
 function drivingAge() {
   let age = Number(prompt('How old are you?'))
   let driveAge = 16
+  // Ask the age of user and define it and age available for drive.
 
   if (age > driveAge) {
     alert("You're old enough to drive.")
@@ -12,3 +13,4 @@ function drivingAge() {
     alert("You're not old enough to drive.")
   }
 }
+// If the age of user is bigger than age available for drive, show a message that you can drive, if not, show a message opposite meaning.
